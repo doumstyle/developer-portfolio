aed676f669c953e2b8c8cf617abec803424f6cf7
Mamadou Ngatté <doumstyle@gmail.com>
2024-03-25 16:44:03 +0100

    fix Next images retrieval config

--- next.config.js
+++ next.config.js
@@ -1,10 +1,21 @@
 const path = require('path')
- 
+
 module.exports = {
   sassOptions: {
     includePaths: [path.join(__dirname, 'styles')],
   },
   images: {
-    domains: ['res.cloudinary.com', 'media.dev.to']
-  }
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'res.cloudinary.com',
+        pathname: '**'
+      },
+      {
+        protocol: 'https',
+        hostname: 'media.dev.to',
+        pathname: '**'
+      },
+    ],
+  },
 }
